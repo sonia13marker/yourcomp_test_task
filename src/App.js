cf0419961cd197dca14370319ed2cc526db41afb
SoNya Marker <soniamarker13@gmail.com>
2023-11-30 20:48:52 +0300

    update SearchPage - add a search input

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './style.scss';
 import './App.css';
 import SearchPage from './pages/SearchPage/SearchPage';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter basename="/yourcomp_test_task">
+        <Header />
         <Routes>
           <Route path='/' element={<SearchPage />} />
           
--- src/pages/SearchPage/SearchPage.jsx
+++ src/pages/SearchPage/SearchPage.jsx
@@ -1,17 +1,35 @@
 import FilterList from '../../components/FilterList/FilterList';
-import Header from '../../components/Header/Header';
 import './SearchPage.scss';
+import search_icon from '../../images/search_icon.svg';
 
 export default function SearchPage () {
     return(
-        <>
-            <Header />
+        <section className='searchPage'>
 
-            <section className='searchWrapper'>
+            <section className='searchPage__wrapper'>
                 <FilterList />
+
+                <div className='searchPage__wrapper__resultWrapper'>
+
+                    <span className='searchPage__wrapper__resultWrapper__searchBar'>
+                        <label className='searchPage__wrapper__resultWrapper__searchBar__label'>
+                            <img src={search_icon} alt="search icon"
+                            className='searchPage__wrapper__resultWrapper__searchBar__label__image'
+                            />
+                            <input type='search' id="searchPage_search" defaultValue="Строительная компания в Москве" 
+                            className='searchPage__wrapper__resultWrapper__searchBar__input'
+                            />
+                        </label>
+                        <p className='searchPage__wrapper__resultWrapper__searchBar__text'>
+                        Найдено <span>31</span> результатов
+                        </p>
+                        
+                    </span>
+
+                </div>
             </section>
 
 
-        </>
+        </section>
     )
 }
